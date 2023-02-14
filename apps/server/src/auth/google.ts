47c76c98b7ab7f9d670be1fa60f7fff1a524d858
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-02-14 17:49:37 +0530

    feat: init prisma and secret sharing part in google auth

--- apps/server/src/auth/google.ts
+++ apps/server/src/auth/google.ts
@@ -1,5 +1,9 @@
 import passport from "passport";
 import google from "passport-google-oauth20";
+import { prisma } from "../lib/db";
+import { Keypair } from "@solana/web3.js";
+import secrets from "secrets.js-grempe";
+import base58 from "bs58";
 import { GID, GSECRET } from "../constant";
 
 export function initgoogle() {
@@ -21,10 +25,51 @@ export function initgoogle() {
           "https://www.googleapis.com/auth/userinfo.email",
         ],
       },
-      (accessToken: any, refreshToken: any, profile: any, done: any) => {
+      async (accessToken: any, refreshToken: any, profile: any, done: any) => {
         console.log(accessToken);
         console.log(refreshToken);
         console.log(profile);
+
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            email: profile.emails[0].value,
+          },
+        });
+
+        if (!existingUser) {
+          const { publicKey, secretKey } = Keypair.generate();
+
+          console.log(base58.encode(secretKey));
+
+          const shares = secrets.share(base58.encode(secretKey), 3, 2);
+
+          console.log(shares);
+
+          // console.log(
+          //   "can combine from 2 shares: ",
+          //   secrets.combine(shares.slice(1, 3))
+          // );
+
+          // console.log(
+          //   "can create new share from 2 shares and combine: ",
+          //   secrets.combine(
+          //     shares
+          //       .slice(1, 2)
+          //       .concat(secrets.newShare(Math.random(), shares.slice(0, 2)))
+          //   )
+          // );
+
+          const newUser = await prisma.user.create({
+            data: {
+              email: profile.emails[0].value,
+              name: profile.displayName,
+              pubkey: publicKey.toBase58(),
+            },
+          });
+
+          console.log("newUser", newUser);
+        }
+
         done(null, profile);
       }
     )
