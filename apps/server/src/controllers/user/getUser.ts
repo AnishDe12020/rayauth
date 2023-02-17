4fed9760c771dc08ae643089b7bde2aab2754a9f
apoorvcodes <tofu.develops@gmail.com>
2023-02-17 15:42:07 +0530

    feat: user fetch done

--- apps/server/src/controllers/user/getUser.ts
+++ apps/server/src/controllers/user/getUser.ts
@@ -1,11 +1,36 @@
 
-// import {SECERET} from "../../constant";
+import {SECERET} from "../../constant";
+import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
-// import { prisma } from "../../../../../packages/shared/db";
-
+import { prisma } from "../../../../../packages/shared/db";
+import { jwtInterface } from "src/interfaces/jwt";
 export function userController() {
   return async (req: Request, res: Response) => {
-       console.log(req.headers.authorization);
+       const auth = req.headers.authorization?.replace("Bearer ", "")
+
+       if (!auth || auth == undefined) {
+        res.status(401).json("Unauthorized")
+        res.end();
+        return
+       }
+       var data = jwt.verify(auth || "", SECERET) as jwtInterface;
+         if (!data) {
+              res.status(401).json("Unauthorized")
+              res.end();
+              return
+
+        }
+        const user = await prisma.user.findUnique({
+            where: {
+                id: data.email
+            }
+        })
+        if (!user) {
+            res.status(404).json("No user was found through this token")
+            res.end();
+            return
+        }
+        res.status(200).json(user)
        res.send("Hello")
   };
 }
---
+++ apps/server/src/interfaces/jwt.ts
@@ -0,0 +1,5 @@
+export interface jwtInterface {
+    email: string
+    id: string
+    time: string
+}
