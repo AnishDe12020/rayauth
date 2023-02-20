66a8c1c0f1368c0c0a3165d64c2860b4a6565f5d
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-02-20 13:50:26 +0530

    chore: delete user helper api route

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,7 +2,7 @@ import env from "dotenv";
 env.config();
 import express, { Express, Request, Response } from "express";
 import session from "express-session";
-import { HOST, PORT } from "./constant";
+import { DB1, DB2, DB3, HOST, PORT } from "./constant";
 import cors from "cors";
 import { initGithub } from "./auth/github";
 import callback from "./controllers/github/callback";
@@ -17,6 +17,9 @@ import proejcts from "./controllers/projects";
 import cookieParser from "cookie-parser";
 import { setQuery } from "./middleware/query";
 import { userController } from "./controllers/user/getUser";
+import { prisma } from "../lib/db";
+import { connect } from "mongoose";
+import { KeyModel } from "./interfaces/key";
 
 const app: Express = express();
 
@@ -51,6 +54,35 @@ app.use("/projects", proejcts);
 app.get("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.send("Hello");
+});
+
+app.post("/delete-user", async (req: Request, res: Response) => {
+  const emailId = req.body.email;
+
+  console.log(emailId);
+
+  await prisma.user.delete({
+    where: {
+      email: emailId,
+    },
+  });
+
+  const mongo1 = await connect(DB1);
+  const key1 = new KeyModel({ email: emailId });
+  await key1.deleteOne();
+  await mongo1.disconnect();
+
+  const mongo2 = await connect(DB2);
+  const key2 = new KeyModel({ email: emailId });
+  await key2.deleteOne();
+  await mongo2.disconnect();
+
+  const mongo3 = await connect(DB3);
+  const key3 = new KeyModel({ email: emailId });
+  await key3.deleteOne();
+  await mongo3.disconnect();
+
+  res.send("User deleted");
 });
 
 app.listen(Number(PORT), HOST, () => {
