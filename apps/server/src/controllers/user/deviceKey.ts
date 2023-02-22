3881cd8e415fd41f6f5737c579e71360927ecb47
apoorvcodes <tofu.develops@gmail.com>
2023-02-22 15:43:28 +0530

    feat: key share

---
+++ apps/server/src/controllers/user/deviceKey.ts
@@ -0,0 +1,33 @@
+import { SECERET } from "../../constant";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { prisma } from "../../../lib/db";
+import { jwtInterface } from "src/interfaces/jwt";
+export function userController() {
+  return async (req: Request, res: Response) => {
+    const auth = req.headers.authorization?.replace("Bearer ", "");
+
+    if (!auth || auth == undefined) {
+      res.status(401).json("Unauthorized");
+      res.end();
+      return;
+    }
+    var data = jwt.verify(auth || "", SECERET) as jwtInterface;
+    if (!data) {
+      res.status(401).json("Unauthorized");
+      res.end();
+      return;
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: data.email,
+      },
+    });
+    if (!user) {
+      res.status(404).json("No user was found through this token");
+      res.end();
+      return;
+    }
+    
+  };
+}
--- apps/server/src/helpers/combine.ts
+++
@@ -1,7 +0,0 @@
-import secrets from "secret-sharing.js";
-import hex from "hexyjs";
-
-export function combineKey(slices: string[]): string {
-  const comb = secrets.combine(slices);
-  return hex.hexToStr(comb) || "";
-}
