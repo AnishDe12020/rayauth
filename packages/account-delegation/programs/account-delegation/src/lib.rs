e988c04015c4955db3acec3d4c597f8a42398d3b
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-02-24 14:16:04 +0530

    feat: add instruction to create delegate account

--- packages/account-delegation/programs/account-delegation/src/lib.rs
+++ packages/account-delegation/programs/account-delegation/src/lib.rs
@@ -1,15 +1,89 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6gJXtZUfzB7tiu7i1PA1tJDJgWSyamuLgdBaSs8QFhNK");
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+// const STRING_LENGTH_PREFIX: usize = 4;
+// const STRING_CHAR_MULTIPLIER: usize = 4;
+// const DB_ID_LENGTH: usize = STRING_LENGTH_PREFIX + (24 * STRING_CHAR_MULTIPLIER);
+// const U64_LENGTH: usize = 8;
+// const I64_LENGTH: usize = 8;
+// const BOOL_LENGTH: usize = 1;
 
 #[program]
 pub mod account_delegation {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_delegated_account(
+        ctx: Context<CreatedDelegatedAccount>,
+        delegates: Vec<Pubkey>,
+    ) -> Result<()> {
+        let delegated_account = &mut ctx.accounts.delegated;
+        let owner = &ctx.accounts.owner;
+        let project_account = &ctx.accounts.project_account;
+
+        let mut delegates = delegates;
+        delegates.sort();
+        delegates.dedup();
+
+        let total_delegate_count = delegates.len();
+        if total_delegate_count == 0 {
+            return Err(ErrorCode::NoDelegatesProvided.into());
+        }
+
+        delegated_account.init(owner.key, project_account.key, delegates);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(delegates: Vec<Pubkey>)]
+pub struct CreatedDelegatedAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>, // owner of the delegated account (can add delegates)
+    #[account(mut)]
+    pub payer: Signer<'info>, // payer of the delegated account (can be owner)
+    /// CHECK: we are not writing to the project account, so we don't need to mark it as mut
+    pub project_account: AccountInfo<'info>, // project account
+    #[account(
+        init,
+        payer = payer,
+        space = DelegatedAccount::SIZE_WITHOUT_DELEGATES + (delegates.len() * PUBLIC_KEY_LENGTH),
+        seeds = [
+            b"delegated_account".as_ref(),
+            project_account.key.as_ref(),
+            owner.key.as_ref(),
+        ],
+        bump,
+    )]
+    pub delegated: Account<'info, DelegatedAccount>, // delegated account
+    pub system_program: Program<'info, System>, // system program
+}
+
+#[account]
+pub struct DelegatedAccount {
+    pub owner: Pubkey,           // owner of the account
+    pub project_account: Pubkey, // project account
+    pub delegates: Vec<Pubkey>,  // delegates allowed to send transactions via this account
+}
+
+impl DelegatedAccount {
+    pub const SIZE_WITHOUT_DELEGATES: usize = DISCRIMINATOR_LENGTH + // discriminator
+        PUBLIC_KEY_LENGTH + // project account
+        PUBLIC_KEY_LENGTH // owner
+        + 4; //buffer, getting a serialization error without thi
+
+    pub fn init(&mut self, owner: &Pubkey, project_account: &Pubkey, delegates: Vec<Pubkey>) {
+        self.owner = *owner;
+        self.project_account = *project_account;
+        self.delegates = delegates;
+    }
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("No delegates provided")]
+    NoDelegatesProvided,
+}
--- packages/account-delegation/tests/account-delegation.ts
+++ packages/account-delegation/tests/account-delegation.ts
@@ -2,15 +2,68 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { AccountDelegation } from "../target/types/account_delegation";
 
+import lumina from "@lumina-dev/test";
+import { expect } from "chai";
+
+lumina();
+
 describe("account-delegation", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.AccountDelegation as Program<AccountDelegation>;
+  const program = anchor.workspace
+    .AccountDelegation as Program<AccountDelegation>;
+  const connection = anchor.getProvider().connection;
+  const userWallet = anchor.workspace.AccountDelegation.provider.wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const project_account = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
+
+  const airdropToPayer = async () => {
+    const sig = await connection.requestAirdrop(payer.publicKey, 1000000000);
+    await connection.confirmTransaction(sig);
+  };
+
+  it("Creates a new delegate account", async () => {
+    await airdropToPayer();
+
+    const [delegatedAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("delegated_account"),
+        project_account.publicKey.toBuffer(),
+        userWallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const createTx = await program.methods
+      .createDelegatedAccount([project_account.publicKey])
+      .accounts({
+        delegated: delegatedAccount,
+        owner: userWallet.publicKey,
+        projectAccount: project_account.publicKey,
+        payer: payer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([payer])
+      .rpc();
+
+    console.log("createTx: ", createTx);
+
+    const delegatedAccountInfo = await program.account.delegatedAccount.fetch(
+      delegatedAccount
+    );
+
+    expect(delegatedAccountInfo).to.not.be.null;
+    expect(delegatedAccountInfo.owner.toBase58()).to.equal(
+      userWallet.publicKey.toBase58()
+    );
+    expect(delegatedAccountInfo.projectAccount.toBase58()).to.equal(
+      project_account.publicKey.toBase58()
+    );
+    expect(delegatedAccountInfo.delegates.length).to.equal(1);
+    expect(delegatedAccountInfo.delegates[0].toBase58()).to.equal(
+      project_account.publicKey.toBase58()
+    );
   });
 });
