10322deb19ead373bac850ccda8d9effc833c89e
apoorvcodes <tofu.develops@gmail.com>
2023-02-25 22:54:46 +0530

    feat: new hook setup

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  clean: true,
+  splitting: true,
+  treeshake: true,
+  minify: true,
+  format: ['cjs', 'esm'],
+  dts: true
+});
