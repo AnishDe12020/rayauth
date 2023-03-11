bb938f0a5d3840537c6a4b4813d0a1dfe374cfed
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-03-11 11:40:53 +0530

    feat: sendTransaction

--- packages/react-legacy/src/hooks/useAuth.tsx
+++ packages/react-legacy/src/hooks/useAuth.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useCookies } from "react-cookie";
 import { authInterface } from "../interfaces/auth";
 import { userConstructor } from "../classes";
@@ -7,33 +7,41 @@ import { useConfig } from "../providers";
 import { BASEURL, WALLET } from "../constants";
 import { walletListener } from "../classes/eventListener";
 import { store } from "../store";
-import { Transaction, VersionedTransaction } from "@solana/web3.js";
+import {
+  clusterApiUrl,
+  Connection,
+  Transaction,
+  VersionedTransaction,
+} from "@solana/web3.js";
 import { sleep } from "../helpers";
-import hex from "hex-array"
-export function useAuth(cookieName: string = "jwt-rayauth"): authInterface {
+import hex from "hex-array";
+
+export function useAuth(cookieName: string = "jwt-rayauth") {
   const config = useConfig();
-  const syncstore = store()
+  const syncstore = store();
   const [user, setUser] = useState<userConstructor | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [cookies, setCookie, removeCookie] = useCookies([cookieName]);
+
+  const connection = useMemo(() => new Connection(clusterApiUrl("devnet")), []);
+
   useEffect(() => {
     const fetchUser = async () => {
-      console.log("CHAL RHA")
+      console.log("CHAL RHA");
       setIsLoading(true);
       if (!cookies[cookieName]) {
         setIsLoading(false);
         return;
       }
-      console.log("YAHA BHI CHAL RHA")
+      console.log("YAHA BHI CHAL RHA");
 
       const user = await getUser(cookies[cookieName], syncstore);
-      console.log("USER CHAL RHA")
+      console.log("USER CHAL RHA");
 
       setUser(user);
 
       setIsLoading(false);
-      console.log("PURA CHAL RHA", user.state)
-
+      console.log("PURA CHAL RHA", user.state);
     };
     fetchUser();
   }, [cookies]);
@@ -65,27 +73,60 @@ export function useAuth(cookieName: string = "jwt-rayauth"): authInterface {
     removeCookie(cookieName);
   };
 
-  function signTransaction(transaction: Transaction | VersionedTransaction) {
+  async function signTransaction(
+    transaction: Transaction | VersionedTransaction
+  ): Promise<Transaction | VersionedTransaction> {
     try {
-      console.log(user)
-      console.log(user?.state)
+      console.log(user);
+      console.log(user?.state);
       console.log("waw");
       const url = new URL(`${WALLET}/sign-transaction`);
+
+      if (
+        transaction instanceof Transaction &&
+        transaction.recentBlockhash === null
+      ) {
+        let blockhash = (await connection.getLatestBlockhash("finalized"))
+          .blockhash;
+
+        transaction.recentBlockhash = blockhash;
+      }
+
       url.searchParams.append(
         "txn",
-       hex.toString(transaction.serialize({ requireAllSignatures: false }))
+        hex.toString(transaction.serialize({ requireAllSignatures: false }))
       );
-      console.log("hex", hex.toString(transaction.serialize({ requireAllSignatures: false })))
-      console.log("url", url.toString())
-       user?.state.setSrc(url.toString());
-       user?.state.setVisible(true);
-       const res = loopTxnData();
-       return res
+      console.log(
+        "hex",
+        hex.toString(transaction.serialize({ requireAllSignatures: false }))
+      );
+      console.log("url", url.toString());
+      user?.state.setSrc(url.toString());
+      user?.state.setVisible(true);
+      const res = loopTxnData();
+      return res;
     } catch (e) {
       console.error(e);
       throw new Error("Can't execute send transaction");
     }
   }
+
+  const sendTransaction = async (
+    transaction: Transaction | VersionedTransaction
+  ) => {
+    try {
+      const signedTransaction = await signTransaction(transaction);
+      console.log("signed", signedTransaction);
+      const signature = await connection.sendRawTransaction(
+        signedTransaction.serialize()
+      );
+
+      return signature;
+    } catch (e) {
+      console.error(e);
+      throw new Error("Can't execute send transaction");
+    }
+  };
 
   async function loopTxnData(): Promise<any> {
     const startTime = new Date().getTime();
@@ -111,7 +152,14 @@ export function useAuth(cookieName: string = "jwt-rayauth"): authInterface {
     }
     return data;
   }
-  return { signIn, signOut, user, isLoading, handleCallback, walletListener, signTransaction };
+  return {
+    signIn,
+    signOut,
+    user,
+    isLoading,
+    handleCallback,
+    walletListener,
+    signTransaction,
+    sendTransaction,
+  };
 }
-
-
