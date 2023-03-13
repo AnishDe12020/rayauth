16a356c447414fb3766685d888ec0e50bec1dc85
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-03-13 11:22:30 +0530

    chore: remove web legacy and web next

--- packages/react-next/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  entryPoints: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  minify: true,
-  sourcemap: true,
-  outfile: "dist/index.js",
-  external: ["react", "react-dom", "tailwindcss"],
-};
