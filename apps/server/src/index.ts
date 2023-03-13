13a159bc8b1cdf4ca57360316956f10ec6c77b1c
apoorvcodes <tofu.develops@gmail.com>
2023-03-13 16:52:53 +0530

    feat: anish bakchod

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -37,7 +37,7 @@ console.log("frontend url: ", FRONTEND_URL);
 initGithub();
 initdiscord();
 initgoogle();
-app.use(cors({ credentials: true, origin: FRONTEND_URL }));
+app.use(cors({ credentials: true, origin: "*" }));
 app.use(setQuery());
 app.use(
   session({
