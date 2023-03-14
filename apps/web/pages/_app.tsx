8c4299adb3f24e6b4bbe0efe47b5a27e4e36525f
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-03-14 16:10:01 +0530

    seo done

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,14 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "sonner";
+
+import { DefaultSeo } from "next-seo";
+import NextNProgress from "nextjs-progressbar";
+
+import SEO from "../seo.config";
 
 type ComponentWithPageLayout = AppProps & {
   Component: AppProps["Component"] & {
@@ -25,6 +24,9 @@ export default function App({ Component, pageProps }: ComponentWithPageLayout) {
       <Script src="/secrets.js" />
 
       <QueryClientProvider client={queryClient}>
+        <NextNProgress color="#6133C1" options={{ showSpinner: false }} />
+        <DefaultSeo {...SEO} />
+
         <Toaster richColors theme="dark" position="bottom-right" />
         {Component.PageLayout ? (
           <Component.PageLayout>
---
+++ apps/web/seo.config.js
@@ -0,0 +1,27 @@
+const defaultSEOConfig = {
+  title: "RayAuth",
+  description: "Solana Onboarding Infrastructure",
+  cannonical: "https://rayauth.com/",
+  openGraph: {
+    type: "website",
+    url: "https://rayauth.com/",
+    site_name: "RayAuth",
+    title: "RayAuth",
+    description: "Solana Onboarding Infrastructure",
+    images: [
+      {
+        url: "https://rayauth.com/og.png",
+        width: 1200,
+        height: 630,
+        alt: "RayAuth's Opengraph Image",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@RayAuthHQ",
+    site: "@RayAuthHQ",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
